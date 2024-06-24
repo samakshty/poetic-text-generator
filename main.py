300d3eae13e06fda159c3ed1a20d7bf13ec1c7c9
Samaksh Tyagi <140980901+samakshty@users.noreply.github.com>
2024-06-25 03:10:27 +0530

    Update main.py
    built neural network (training the model)

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
+import certifi
+import os
+
+# Set the environment variable for SSL certificate file to certifi's CA bundle path
+os.environ['SSL_CERT_FILE'] = certifi.where()
+# --------------------MAIN CODE--------------------
+
 import numpy as np
 import random
 import tensorflow as tf
 
-from tenserflow.keras.model import Sequential #for our model
-from tenserflow.keras.layers import LSTM, Dense, Activation  #for our layers
-from tenserflow.keras.optimizers import RMSprop # for optimisation during the compilation o four model
+from tensorflow.keras.models import Sequential #for our model
+from tensorflow.keras.layers import LSTM, Dense, Activation  #for our layers. LSTM is a type of RNN that is capable of learning long-term dependencies.
+from tensorflow.keras.optimizers import RMSprop # for optimisation during the compilation o four model
 
 # Importing File locally 
-filepath = tf.keras.utils.get_file("shakespear.txt", '/Users/samakshtyagi/VS Code/Python/shakespear_text_generator /shakespeare.txt')
+filepath = tf.keras.utils.get_file("shakespear.txt", 'https://storage.googleapis.com/download.tensorflow.org/data/shakespeare.txt')
 
 # Reading file and converting into numerical data for our neural network
 text = open(filepath, 'rb').read().decode(encoding='utf-8').lower()
@@ -44,8 +51,8 @@ for i in range(0, len(text) - SEQ_LENGTH, STEP_SIZE):
     next_char.append(text[i + SEQ_LENGTH])   # next character is the one at 5th index.
     
 # Creating 2 numpy arrays with zeroes
-x = np.zeros((len(sentences), SEQ_LENGTH, len(characters)), dtype=np.bool)
-y = np.zeros((len(sentences), len(characters)), dtype=np.bool)
+x = np.zeros((len(sentences), SEQ_LENGTH, len(characters)), dtype=np.bool_)
+y = np.zeros((len(sentences), len(characters)), dtype=np.bool_)
 # Whenever in a specific sentence, at a specific position, a speciifc char occurs, we will set it to True/1. all other values remain 0.
 
 # dimensions: 1. for all the possible sentences that we have. 
@@ -63,4 +70,28 @@ for i, sentz in enumerate(sentences): #taking all sentences and assigning them a
 #Data is prepared now. 
 
 # next step : building recurrent neural network
-# tbd
+model = Sequential()  # this initialized new instance of sequential model in keras
+model.add(LSTM(128, input_shape = (SEQ_LENGTH, len(characters)))) 
+# The input will fall into our LSTM layer with 128 neurons. Input shape is length of sentence times the amount of character.
+# The character which shall follow will be set to True or False. 
+model.add(Dense(len(characters))) 
+# Dense is a hidden layer, jsut to increase complexity. It has as many neurons as our possible '(characters)'
+model.add(Activation('softmax')) 
+# Softmax Activation function scales the output so all values add up to one (100%). (This gives probability for each character. The output is alwasy of how likely a certain char is going to be the next character. Eg 'o' 70% then 'k will be 100-70=30%)
+
+# Compiling Model now: 
+model.compile(loss='categorical_crossentropy', optimizer=RMSprop(learning_rate=0.01))
+# loss function is categorical crossentropy, optimizer is RMSprop with learning rate of 0.01
+# RMSprop is a popular optimizer for RNNs. It adapts the learning rate for each
+
+model.fit(x, y, batch_size=256, epochs=4) 
+# we fitted the model on training data (x,y) , 
+# batch size = how many examples we are gonna put in the network at once. 
+# epoch = how many times our network will see the same data over and ober again 
+
+model.save('textgenerator.model')
+
+#Run File and model will start training.
+
+#NEXT STEP: use trained model and generate text
+#tbd
